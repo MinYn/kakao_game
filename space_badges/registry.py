ced5912be83e753dbf0727987c845db0a8f96263
Yoon SeokMin <ysmysm3@naver.com>
2026-07-14 16:58:40 +0900

    Apply cute badge background frame and names

--- space_badges/registry.py
+++ space_badges/registry.py
@@ -20,14 +20,14 @@ class BadgeVariant:
 
 
 VARIANTS: list[BadgeVariant] = [
-    BadgeVariant(name="ORION-X", sub="FLT-101", shape=ShipShape.SHUTTLE, color="white"),
-    BadgeVariant(name="TITAN-V", sub="HEAVY", shape=ShipShape.ROCKET, color="silver"),
-    BadgeVariant(name="AETHER", sub="MK-IV", shape=ShipShape.INTERCEPTOR, color="dark"),
-    BadgeVariant(name="VOYAGER", sub="DEEP", shape=ShipShape.SHUTTLE, color="gold"),
-    BadgeVariant(name="ZENITH", sub="APOLLO", shape=ShipShape.ROCKET, color="white"),
-    BadgeVariant(name="ECLIPSE", sub="STEALTH", shape=ShipShape.INTERCEPTOR, color="black"),
-    BadgeVariant(name="ATLAS", sub="CARGO", shape=ShipShape.LIFTER, color="orange"),
-    BadgeVariant(name="NOVA", sub="PRIME", shape=ShipShape.SHUTTLE, color="blue"),
-    BadgeVariant(name="HELIOS", sub="SOLAR", shape=ShipShape.ROCKET, color="gold"),
-    BadgeVariant(name="VANGUARD", sub="SSTO", shape=ShipShape.LIFTER, color="silver"),
+    BadgeVariant(name="MOMO-1", sub="BEEP", shape=ShipShape.SHUTTLE, color="white"),
+    BadgeVariant(name="BERRY", sub="POP", shape=ShipShape.ROCKET, color="silver"),
+    BadgeVariant(name="BUBBLE", sub="BOO", shape=ShipShape.INTERCEPTOR, color="dark"),
+    BadgeVariant(name="COOKIE", sub="YUM", shape=ShipShape.SHUTTLE, color="gold"),
+    BadgeVariant(name="PUDDING", sub="PON", shape=ShipShape.ROCKET, color="white"),
+    BadgeVariant(name="CANDY", sub="NOM", shape=ShipShape.INTERCEPTOR, color="black"),
+    BadgeVariant(name="PEACHY", sub="CARGO", shape=ShipShape.LIFTER, color="orange"),
+    BadgeVariant(name="NOVA-BUN", sub="HOP", shape=ShipShape.SHUTTLE, color="blue"),
+    BadgeVariant(name="SUNNY", sub="SOL", shape=ShipShape.ROCKET, color="gold"),
+    BadgeVariant(name="JELLY", sub="SSTO", shape=ShipShape.LIFTER, color="silver"),
 ]
